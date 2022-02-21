c8d040ff59083b00e830ad583ad0494bb1fb8fe7
Prafullsingh975 <96352051+Prafullsingh975@users.noreply.github.com>
2022-02-21 18:41:29 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,7 @@
+public class Program2 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 5;
+        System.out.println("Sum of " + a + " & " + b + " = " + (a + b));
+    }
+}
