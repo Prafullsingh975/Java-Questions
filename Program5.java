0c4b8f0f3d88a323ddfcc6284f0ced66bc18a1c4
Prafullsingh975 <96352051+Prafullsingh975@users.noreply.github.com>
2022-02-22 12:12:59 +0530

    Add files via upload

---
+++ Program5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Program5 {
+    public static void main(String[] args) {
+        /*Accept the length and width of a rectangle.
+        Calculate & print the area and perimeter.*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Length");
+        double l = sc.nextDouble();
+        System.out.println("Enter Breath");
+        double b = sc.nextDouble();
+        System.out.println("Area of rectangle = " + (l*b));
+        double p = 2*(l+b);
+        System.out.println("Perimeter of rectangle = " + p);
+//        System.out.println("Perimeter of rectangle = "+ (2*(l+b)));
+    }
+}
