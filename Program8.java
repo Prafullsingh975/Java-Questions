2cbb28da5f7ee4ec043556a25bd235c5de87eaa3
Prafullsingh975 <96352051+Prafullsingh975@users.noreply.github.com>
2022-02-23 10:28:58 +0530

    Add files via upload

---
+++ Program8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Program7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        Write a program to accept temperature in Fahrenheit & convert into Celsius.
+        (Look for the formula on internet)
+        */
+        System.out.println("Enter temperature in Fahrenheit ");
+        float tem = sc.nextFloat();
+        float celsius = (tem - 32) * 5/9;
+        System.out.println("Temperature " + celsius + "℃");
+    }
+}
---
+++ Program9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Program8 {
+    public static void main(String[] args) {
+        /*
+        Accept the Principle amount, time & rate of interest
+        and calculate the Simple Interest
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Principle Amount");
+        float Principle = sc.nextFloat();
+        System.out.println("Enter rate of interest per annum ");
+        float rate = sc.nextFloat();
+        System.out.println("Enter time in year ");
+        float time = sc.nextFloat();
+        float SI = Principle * rate/100 * time;
+        System.out.println("Simple Interest is " + SI);
+    }
+}
