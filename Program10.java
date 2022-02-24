ac71c79a8bf826cab69b85324da16fbe0f41db45
Prafullsingh975 <96352051+Prafullsingh975@users.noreply.github.com>
2022-02-24 14:45:28 +0530

    Add files via upload

---
+++ Program10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Program9 {
+    public static void main(String[] args) {
+        /*
+        Take 3 int inputs from user and check and print the result.
+	    all are equal
+	    any of two are equal
+	    ( use && || )
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a==b && b==c && c==a ){
+            System.out.println("All are equal");
+            System.out.println("i.e. " + a + "=" + b + "=" + c);
+        }
+        else if (a==b || b==c || a==c){
+            System.out.println("Any of two are equal");
+        }
+        else {
+            System.out.println("All three are unequal");
+        }
+
+    }
+}
