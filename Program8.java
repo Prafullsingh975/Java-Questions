a2c03e72e9abe31b7ce519a5850953f1fd7a772e
Prafullsingh975 <96352051+Prafullsingh975@users.noreply.github.com>
2022-02-25 17:32:12 +0530

    Update Program8.java

--- Program8.java
+++ Program8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Program7 {
+public class Program08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         /*
