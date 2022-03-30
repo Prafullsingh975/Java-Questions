262547503d682961b8da5e98ddd0c69632237e7c
Prafull Singh <96352051+Prafullsingh975@users.noreply.github.com>
2022-03-30 17:24:58 +0530

    Triangle

---
+++ Program50.java
@@ -0,0 +1,22 @@
+public class Program50 {
+    public static void main(String[] args) {
+        /*
+        Triangle
+	    *
+	   * *
+	  * * *
+	 * * * *
+	* * * * *
+         */
+        int n = 5;
+        for (int i = 1;i<=n;i++){
+            for (int k = 1;k<=n-i;k++)
+                System.out.print(" ");
+            for (int j = 1;j<=i;j++)
+                System.out.print("* ");
+//            for (int l = 1;l<=k;l++)
+//                System.out.print("  ");
+            System.out.println();
+        }
+    }
+}
